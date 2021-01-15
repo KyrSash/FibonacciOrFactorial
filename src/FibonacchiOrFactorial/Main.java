00c0e401a43703707d5cd5ca9ca22608c2cbd524
kyrsash <tolstikan@gmail.com>
2021-01-15 21:23:14 +0300

    Fibonacci or Factorial

--- src/FibonacchiOrFactorial/Main.java
+++ src/FibonacchiOrFactorial/Main.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //FibonacchiOrFactorial fibonacchiOrFactorial = new FibonacchiOrFactorial();
-        //System.out.println(Arrays.toString(fibonacchiOrFactorial.getFibonacchiOrFactorial(1, 3, 5)));
+        FibonacchiOrFactorial fibonacchiOrFactorial = new FibonacchiOrFactorial();
+        System.out.println(Arrays.toString(fibonacchiOrFactorial.getFibonacchiOrFactorial(1, 3, 5)));
         //System.out.println(Arrays.toString(fibonacchiOrFactorial.getFibonacchiOrFactorial(2, 1, 5)));
 
 //        Factorial factorial = new Factorial();
